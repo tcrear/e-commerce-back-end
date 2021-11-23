6f0392b7d63536e300f1ce6fc6726e61e8501f45
tcrear <smileybear88@hotmail.com>
2021-11-23 13:22:05 -0600

    updating category-routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -28,9 +28,9 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  try {
-    const categoryData = await Category.create(req.body)
+router.post('/', async (req, res) => {
+  try{
+    const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
